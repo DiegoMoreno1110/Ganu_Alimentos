e31836f0a4d0caf9d188df3d76a02423f5e103ad
Yair Pimentel <42229163+Yair0@users.noreply.github.com>
2020-05-12 15:12:54 -0500

    Menu en Cart

--- app/src/main/java/com/dmr/ganu_alimentos/cart/CartActivity.kt
+++ app/src/main/java/com/dmr/ganu_alimentos/cart/CartActivity.kt
@@ -3,10 +3,17 @@ package com.dmr.ganu_alimentos.cart
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dmr.ganu_alimentos.R
+import kotlinx.android.synthetic.main.app_bar.*
 
 class CartActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
+
+
 }
