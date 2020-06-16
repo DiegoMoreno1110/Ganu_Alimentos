aad6d1947c751bc651462f99847746d17e644be9
DiegoMoreno <moreno.diego98@live.com.mx>
2020-06-15 22:09:08 -0500

    Create OrderFireBase.kt

---
+++ app/src/main/java/com/dmr/ganu_alimentos/OrderFireBase.kt
@@ -0,0 +1,33 @@
+package com.dmr.ganu_alimentos
+
+import com.dmr.ganu_alimentos.model.CartItem
+import com.dmr.ganu_alimentos.model.Order
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class OrderFireBase {
+
+    private lateinit var database: DatabaseReference
+    private lateinit var key: String
+    private var firebaseUser: FirebaseUser? =  null
+
+
+    constructor(){
+        database = FirebaseDatabase.getInstance().reference
+    }
+
+
+    fun addOrder(order: Order){
+        //val key = database.child("cart").push().key
+        val userID = FirebaseAuth.getInstance().currentUser!!.uid
+        val itemOrderList = database.child("order").child(userID!!).push()
+        order.id = itemOrderList.key
+        itemOrderList.setValue(order)
+
+
+
+    }
+
+}
